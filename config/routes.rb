ad3c53c45d0a3c48f2b52450ebcf8f67f8895ae0
michellemoon236 <michellemoon236@gmail.com>
2019-07-15 14:21:40 -0700

    create resources for project, task, user_project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :user_projects
+  resources :tasks
+  resources :projects
   devise_for :users, :controllers => {registrations: 'registrations' }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'application#home'
