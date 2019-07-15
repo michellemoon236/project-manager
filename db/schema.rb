29214c37bd91bc325dfeec81146e1d368a1efd78
michellemoon236 <michellemoon236@gmail.com>
2019-07-15 11:56:12 -0700

    add nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_07_15_180429) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
