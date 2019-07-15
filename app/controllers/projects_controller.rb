ba141455fc90ef3beb93a41d45bd0ed6b3042452
michellemoon236 <michellemoon236@gmail.com>
2019-07-15 14:43:04 -0700

    add new and create routes to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,29 @@
 class ProjectsController < ApplicationController
+  def new
+    @project = Project.new 
+    8.times do 
+      @project.tasks.build
+    end
+  end
+
+  def create 
+    @project = Project.create(project_params)
+    if @project.save
+      redirect_to project_path(@project)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(
+      :name,
+      :description,
+      tasks_attributes:[:content, :complete, :project_id, :id],
+      user_ids:[]
+    )
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_194253) do
+ActiveRecord::Schema.define(version: 2019_07_15_212018) do
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer "project_id"
+    t.string "content"
+    t.boolean "complete", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_projects", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
