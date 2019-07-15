85b294ad8732e0a7f3d2a5a4491fb26251e10c1e
michellemoon236 <michellemoon236@gmail.com>
2019-07-15 12:24:52 -0700

    add custom login logout routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'application#home'
+
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+  end
+
+  devise_scope :user do
+    delete 'logout', to: 'devise/sessions#destroy'
+  end
+
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+
 end
