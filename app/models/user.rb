d5b00b2af1b537a5f1f82c6c1e9a31bc408cb993
michellemoon236 <michellemoon236@gmail.com>
2019-07-15 14:24:04 -0700

    add active record relationships in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  has_many :user_projects
+  has_many :projects, through: :user_projects
+  has_many :tasks, through: :projects
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
-
+  
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
--- app/models/user_project.rb
+++ app/models/user_project.rb
@@ -1,2 +1,4 @@
 class UserProject < ApplicationRecord
+  belongs_to :user 
+  belongs_to :project
 end
