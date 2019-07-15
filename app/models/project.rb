bcadacbebc50e85f3b2ddd9e29f3a4c4424db852
michellemoon236 <michellemoon236@gmail.com>
2019-07-15 15:05:27 -0700

    add accepts nested attributes

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,5 @@ class Project < ApplicationRecord
   has_many :user_projects
   has_many :users, through: :user_projects
   has_many :tasks
+  accepts_nested_attributes_for :tasks, reject_if: proc { |attributes| attributes['content'].blank? }
 end
