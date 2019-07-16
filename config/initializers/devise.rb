17639b00f87b9320e395f0cf77a6c88e5068dbfc
michellemoon236 <michellemoon236@gmail.com>
2019-07-16 12:45:55 -0700

    update oauth env file

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,9 +260,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :github, '76f70ecbe4e0c5029ee3', '1ed7dd0f133f3a18a9f37a5af3b2ab9f8eac78da', scope: 'user:email'
-  # config.omniauth :github, '76f70ecbe4e0c5029ee3', '1ed7dd0f133f3a18a9f37a5af3b2ab9f8eac78da'
-  
+
+  config.omniauth :github, 'GITHUB_KEY', 'GITHUB_SECRET', scope: 'user:email'
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
