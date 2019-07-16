d12e7d792e3759e92ca53357b306b7b9c1fa6d6c
michellemoon236 <michellemoon236@gmail.com>
2019-07-16 15:27:59 -0700

    task new and create actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,22 @@
 class TasksController < ApplicationController
 
+  def new
+    @task = Task.new(project_id: params[:project_id])
+  end
+
+  def create 
+    @task = Task.create(task_params)
+    redirect_to project_path(@task.project_id)
+  end
+
   def task_complete
     @task = Task.find(params[:task][:id])
     @task.complete = true
     @task.save
     redirect_to project_path(id: @task.project_id)
+  end
+
+  def task_params
+    params.require(:task).permit(:content, :project_id)
   end
 end
