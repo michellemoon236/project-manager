9523489108ea1d8b155c74ab68057b3148f9e09a
michellemoon236 <michellemoon236@gmail.com>
2019-07-16 15:27:12 -0700

    add task complete method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,9 @@
 class TasksController < ApplicationController
+
+  def task_complete
+    @task = Task.find(params[:task][:id])
+    @task.complete = true
+    @task.save
+    redirect_to project_path(id: @task.project_id)
+  end
 end
